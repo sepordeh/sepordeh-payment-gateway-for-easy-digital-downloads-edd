77612091f995ca521bb7f8c838786e81d294461f
IOTACO <62711192+IOTACO@users.noreply.github.com>
2021-10-14 23:37:03 +0330

    Add files via upload

---
+++ edd-sepordeh.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Sepordeh for EDD
+ * Author: ehsaan
+ * Description: این افزونه، درگاه پرداخت آنلاین <a href="https://Sepordeh.com">سپرده</a> را برای افزونه‌ی Easy Digital Downloads فعال می‌کند.
+ * Version: 2.1.0
+ * Author URI: https://Sepordeh.com/
+ */
+
+// Toman Currency
+require 'includes/toman-currency.php';
+
+// Include the main file
+require 'gateways/sepordeh.php';
