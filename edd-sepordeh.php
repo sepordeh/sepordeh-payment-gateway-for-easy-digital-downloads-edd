8788d0189005a24f3c22feda6947c385b92af50b
IOTACO <62711192+IOTACO@users.noreply.github.com>
2021-10-18 01:28:09 +0330

    Update edd-sepordeh.php

--- edd-sepordeh.php
+++ edd-sepordeh.php
@@ -4,8 +4,15 @@
  * Author: Sepordeh
  * Description: This plugin activate <a href="https://Sepordeh.com">Sepordeh</a> payment method in EDD
  * Version: 3.0.0
+ * Plugin URI: https://wordpress.org/plugins/edd-sepordeh
+ * Author: Sepordeh.com
  * Author URI: https://Sepordeh.com/
  */
+
+function edd_sepordeh_load_textdomain() {
+	load_plugin_textdomain( 'edd-sepordeh', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+add_action( 'init', 'edd_sepordeh_load_textdomain' );
 
 // Toman Currency
 require 'includes/toman-currency.php';
